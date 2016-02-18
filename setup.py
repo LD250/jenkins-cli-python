686e96d7fae089f97bb15002ee8d9c2fbc8415cc
Bradley Cicenas <bradley.cicenas@gmail.com>
2016-02-18 18:00:47 -0500

    -a add license to setup.py

--- setup.py
+++ setup.py
@@ -9,15 +9,17 @@ requires = [ 'pbr>=1.3.0',
              'python-jenkins>=0.4.8',
              'six>=1.9.0' ]
 
-setup(name='jenkins-cli',
-      version='0.1',
-      description='Commandline interface for Jenkins',
-      long_description=README,
-      author='Denys Levchenko',
-      keywords='jenkins, commandline, cli',
-      packages=find_packages(),
-      install_requires=requires,
-      entry_points = {
-        'console_scripts' : [ 'jenkins-cli = jenkins_cli:main' ]
-      }
+setup(
+    name='jenkins-cli',
+    version='0.1',
+    description='Commandline interface for Jenkins',
+    long_description=README,
+    author='Denys Levchenko',
+    keywords='jenkins, commandline, cli',
+    license='http://opensource.org/licenses/MIT',
+    packages=find_packages(),
+    install_requires=requires,
+    entry_points = {
+      'console_scripts' : [ 'jenkins-cli = jenkins_cli:main' ]
+    }
 )
